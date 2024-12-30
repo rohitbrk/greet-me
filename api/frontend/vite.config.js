cae284405f3db9b19caac7cd93f2a2e857a88ff2
Rohith <rohithkumarbathula99@gmail.com>
2024-12-30 08:25:25 +0530

    change prod.yaml workflow5

--- api/frontend/vite.config.js
+++ api/frontend/vite.config.js
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "../api/public", // Replace with your desired directory name
-  },
 });
