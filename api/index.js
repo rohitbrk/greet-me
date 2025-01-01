20b134dde6596f3dab863a11842cf88658ab262d
Rohith <rohithkumarbathula99@gmail.com>
2025-01-01 18:38:43 +0530

    change prod yaml github workflow file 4

--- api/index.js
+++ api/index.js
@@ -2,14 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3001;
 const cors = require("cors");
+const path = require("path");
 
 app.use(cors());
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
-// Serve static files from the "public" directory
-app.use(express.static("../frontend/dist"));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+});
 
 // Handle POST requests to "/greet"
 app.post("/api/greet", (req, res) => {
